7d725cc2d57b5bdb6503c7b16f39b83f7693c25d
Ivan Fonseca <ivansafonseca@hotmail.com>
2026-07-22 14:14:33 +0000

    changed category_pk to category_id column name

--- models/staging/erp/stg_erp__category.sql
+++ models/staging/erp/stg_erp__category.sql
@@ -7,7 +7,7 @@ with
     /* Renaming and casting */
     , renamed as (
         select
-            cast(productcategoryid as int) as category_pk
+            cast(productcategoryid as int) as category_id
             , cast(name as string) as category_name
             , cast(rowguid as string) as category_row_guid
             , cast(modifieddate as date) as category_modified_date
